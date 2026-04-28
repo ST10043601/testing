7eb5033aa7601ac903ec823ddcbaa78ad4ae2dea
ST10043601 <161279701+ST10043601@users.noreply.github.com>
2026-04-28 13:38:00 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id 'org.jetbrains.kotlin.android' version '1.9.x'  // Specify explicit version
+    kotlin("android") version "1.9.x"
 }
 
 android {
