577dd81439193ab6df8116309841a51fb10a15a0
ST10043601 <161279701+ST10043601@users.noreply.github.com>
2026-04-28 13:31:54 +0200

    Change minSdk version from 24 to 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.testing"
-        minSdk = 24
+        minSdk = 17
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
